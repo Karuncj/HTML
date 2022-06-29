79c7dcef54093600d282d58e4757b8d43da66da4
Karuncj <83808984+Karuncj@users.noreply.github.com>
2022-06-29 21:08:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const formData = document.forms['MyForm']
+const content = document.getElementById("table-content")
+
+function formsfunc() {
+  var name = formData['Name'].value
+  var mail = formData['Mail'].value
+  var pass = formData['password'].value
+  var row="<tr"
+  content.innerHTML="<tr><td>"+name+"</td><td>"+mail+"</td><td>"+pass+"</td></tr>"
+}
